425061dcce3ad76e7b03d67b949964a6507a14dd
Borja Clemente <borja.clemente@gmail.com>
2024-10-21 17:40:38 +0200

    Add gruvbox theme
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

---
+++ themes/neovim/gruvbox.lua
@@ -0,0 +1,14 @@
+local cmd = vim.cmd
+
+-- Transparent BG
+-- Focused pane/window
+cmd('autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE')
+-- All other panes and overlays
+cmd('autocmd ColorScheme * highlight NormalNC ctermbg=NONE guibg=NONE')
+cmd('autocmd ColorScheme * highlight NormalSB ctermbg=NONE guibg=NONE')
+
+require('gruvbox').setup({
+	transparent_mode = true,
+	contrast = "soft"
+})
+cmd.colorscheme("gruvbox")
---
+++ themes/osx/gruvbox.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+BACKGROUND=themes/osx/wallpaper/rocky-desert.webp
